35ec8e409affc48a45700b6c10872dadb1d83146
Aimen Zairi <42500714+ZairiAimenDz@users.noreply.github.com>
2021-04-11 15:23:50 +0200

    Test

--- app/src/main/java/com/zcdev/demo2021/MainActivity.kt
+++ app/src/main/java/com/zcdev/demo2021/MainActivity.kt
@@ -15,7 +15,7 @@ class MainActivity : AppCompatActivity() {
 
 
 
-
+    //Test
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
